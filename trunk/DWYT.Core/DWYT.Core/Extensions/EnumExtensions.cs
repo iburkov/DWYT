7060480be938a841b6796b3d5237f4670d63b4b9
Igor Burkovych <burkovych.igor@gmail.com>
2013-03-23 13:15:49 +0200

    Added role permission enums and enum extesions

---
+++ trunk/DWYT.Core/DWYT.Core/Extensions/EnumExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DWYT.Core.Extensions
+{
+    /// <summary>
+    /// Provides extension methods for System.Enum
+    /// </summary>
+    public static class EnumExtensions
+    {
+        /// <summary>
+        /// Converts emuneration to string value.
+        /// </summary>
+        /// <returns>Name of enumeration.</returns>
+        public static string ConvertToString(this Enum enumeration)
+        {
+            return Enum.GetName(enumeration.GetType(), enumeration);
+        }
+
+        /// <summary>
+        /// Converts string value to enumeration.
+        /// </summary>
+        /// <returns>Member of enumeration.</returns>
+        public static T ConvertToEnum<T>(this string str) where T : struct
+        {
+            return (T)Enum.Parse(typeof(T), str);
+        }
+    }
+}
